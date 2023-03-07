18dca2b0c65a82d1634d4c2a8fc1a48ad2747c31
Onurcan Sever <onsever@icloud.com>
2023-03-06 22:04:25 -0500

    customized accessdeniedexception response

--- src/main/java/com/onurcansever/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/onurcansever/blog/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -58,6 +59,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errors.put("statusCode", HttpStatus.BAD_REQUEST.value());
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    // Handle AccessDeniedException and send the custom error response to the client.
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException exception, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(new Date(), exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
 
