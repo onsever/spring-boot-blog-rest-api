616c48653c457ea9955bb1ac3000fd2f1e1abda6
Onurcan Sever <onsever@icloud.com>
2023-03-06 21:59:55 -0500

    implemented role-based authentication

--- src/main/java/com/onurcansever/blog/config/SecurityConfig.java
+++ src/main/java/com/onurcansever/blog/config/SecurityConfig.java
@@ -2,23 +2,66 @@ package com.onurcansever.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity // Enabling Method-level Security
 public class SecurityConfig {
+
+    // Password Encoder
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // Configuring Security Filter Chain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Enable HTTP Basic Authentication
+        // Restricting access to the API endpoints. (Role-based Authentication)
         http.csrf().disable()
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests((authorize) ->
+                        // Allow all the users to access the API with GET endpoints.
+                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .anyRequest().authenticated()
+                )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+    // Creating users and storing them in an in-memory object.
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Creating an admin user
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        // Creating a regular user
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        // Returning the instance of InMemoryUserDetailsManager
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 }
 
 // Form-based Authentication
+// Secure REST API using in-memory authentication (Role-based Authentication)
+// Spring Security won't use a plain text as a password, it will use the password encoder to encode the password.
